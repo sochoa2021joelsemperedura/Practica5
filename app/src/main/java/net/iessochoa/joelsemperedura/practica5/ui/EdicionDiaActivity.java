1164343e0fc5b18cedf9d487722e56d3e38f4362
Joel Sempere <joelsempere96@gmail.com>
2021-12-30 13:18:16 +0100

    Terminando Parte 1 - hacer la parte visual de la mainActivity

--- app/src/main/java/net/iessochoa/joelsemperedura/practica5/ui/EdicionDiaActivity.java
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica5/ui/EdicionDiaActivity.java
@@ -2,26 +2,44 @@ package net.iessochoa.joelsemperedura.practica5.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import net.iessochoa.joelsemperedura.practica5.R;
 import net.iessochoa.joelsemperedura.practica5.model.DiaDiario;
 
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class EdicionDiaActivity extends AppCompatActivity {
+    public static String EXTRA_EDICION_DIA = "EdicionDiaActivity.diaDiario"; // el objeto diaDiario que llega
+
+    DiaDiario diaDiario;
+    Intent iBack; //Devolver a la actividad principal el intent
+    Date newFecha; ///guardar la fecha fuera del OnClickFecha
+
     Spinner spValoracion;
     Button btnFecha;
     TextView tvFecha;
+    Button btnGuardar;
+    EditText etResumenBreve;
+    EditText etDiarioTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +52,78 @@ public class EdicionDiaActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence>adaptadorValoracion = ArrayAdapter.createFromResource(this,
                 R.array.valoracion, android.R.layout.simple_spinner_item);
         spValoracion.setAdapter(adaptadorValoracion);
-        //posicion del valor por defecto del spinner
-        spValoracion.setSelection(5);
+
+        iBack = getIntent(); //recupera el intent y verifica si es nuevo o existente
+        if (iBack.getParcelableExtra(EXTRA_EDICION_DIA) == null){
+            //posicion del valor por defecto del spinner
+            spValoracion.setSelection(5);
+        }else{
+            //TODO añadir todos los datos a los campos correspondientes
+            this.diaDiario = iBack.getParcelableExtra(EXTRA_EDICION_DIA); //recupero objeto
+            spValoracion.setSelection(adaptadorValoracion.getPosition(String.valueOf(diaDiario.getValoracionDia()))); //valoracion del objeto
+        }
+
 
         //Boton fecha abrira un DatePickerDialog
         btnFecha.setOnClickListener(e->{
             onClickFecha();
         });
 
+        //Operaciones realizadas al pulsar el boton guardar
+        btnGuardar.setOnClickListener(e->{
+            if (compruebaCampos()){
+               if (diaDiario == null){ //si es un dia nuevo
+                    iBack.putExtra(EXTRA_EDICION_DIA,new DiaDiario(
+                            newFecha,Integer.parseInt(spValoracion.getSelectedItem().toString()),
+                            etResumenBreve.getText().toString(),etDiarioTexto.getText().toString()));
+                }else{ //si ya existe
 
+                    //TODO
+
+                }
+                setResult(RESULT_OK,iBack);
+                finish();
+            } else{
+               abrirDialogo();
+            }
+        });
+
+
+    }
+    // en caso de faltar algun campo por rellener abre un dialogo
+    private void abrirDialogo() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.stAlerta)
+                .setMessage(R.string.stMensajeAlertDialog)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //no hace nada, solo avisa
+                    }
+                }).show();
+
+    }
+
+    //comprueba que el objeto puede ser creado y enviado a la activity principal
+    private boolean compruebaCampos() {
+        Boolean resultado = true;
+
+        if (tvFecha.getText().toString().equals("")||
+        etResumenBreve.getText().toString().equals("")||
+        etDiarioTexto.getText().toString().equals("")){
+            resultado = false;
+        }
+
+        return resultado;
     }
 
     public void iniciaViews(){
         spValoracion = findViewById(R.id.spValoracion);
         btnFecha = findViewById(R.id.btnFecha);
         tvFecha = findViewById(R.id.tvFecha);
+        btnGuardar = findViewById(R.id.btnGuardar);
+        etResumenBreve = findViewById(R.id.etResumenBreve);
+        etDiarioTexto = findViewById(R.id.etDiarioTexto);
     }
 
     //Nos abre el click del boton fecha
@@ -66,6 +141,7 @@ public class EdicionDiaActivity extends AppCompatActivity {
 
                         //Asignacion de la fecha seleccionada en el dialogo al textView
                         tvFecha.setText(DiaDiario.getFechaEstaticaFormatoLocal(fecha));
+                        newFecha = fecha;
                     }
                 }, newCalendar.get(Calendar.YEAR),
                 newCalendar.get(Calendar.MONTH),
--- app/src/main/java/net/iessochoa/joelsemperedura/practica5/ui/MainActivity.java
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica5/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package net.iessochoa.joelsemperedura.practica5.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -7,13 +8,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import net.iessochoa.joelsemperedura.practica5.R;
 import net.iessochoa.joelsemperedura.practica5.model.DiaDiario;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int OPTION_REQUEST_NUEVA = 0; //nuevo día
+    public static final int OPTION_REQUEST_MODIFICAR = 1; //modificar día
+
+    FloatingActionButton fabAnyadir;
     Toolbar toolbar;
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +33,9 @@ public class MainActivity extends AppCompatActivity {
         //Asignar Toolbar a la actividad
         setSupportActionBar(toolbar);
 
-        // PRUEBA TODO ELIMINAR
-
-        button.setOnClickListener(e->{
+       fabAnyadir.setOnClickListener(e->{
           Intent intent = new Intent(MainActivity.this, EdicionDiaActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent,OPTION_REQUEST_NUEVA); //probando, main aun no realizado
         });
 
     }
@@ -42,7 +47,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void iniciaViews(){
+        fabAnyadir = findViewById(R.id.fabAnyadir);
         toolbar = findViewById(R.id.toolbar);
-        button = findViewById(R.id.btnPrueba);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_CANCELED){
+            DiaDiario diaDiario;
+            diaDiario = (DiaDiario)data.getParcelableExtra(EdicionDiaActivity.EXTRA_EDICION_DIA);
+            switch (requestCode){
+                case OPTION_REQUEST_NUEVA:
+
+                    break;
+                case OPTION_REQUEST_MODIFICAR:
+                    break;
+            }
+        }
     }
 }
